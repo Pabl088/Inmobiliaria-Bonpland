83fbdd59b9933eb24a7e38b94cc97d2820a35f0c
Pabl088 <pnaf88@gmail.com>
2023-01-09 19:13:39 -0300

    add spring rest structure

---
+++ api/src/test/java/com/bonpland/bonpland/BonplandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonpland.bonpland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonplandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
